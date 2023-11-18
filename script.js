1c811b900c74c1da4d0d107644259e367382a1c5
Erdni Zungruev <shagidzun@gmail.com>
2023-11-18 17:32:32 +0300

    feat: implement form submission with mockFetch()

---
+++ script.js
@@ -0,0 +1,52 @@
+function mockFetch(url, options) {
+  return Promise.resolve({
+      json: () => Promise.resolve({ message: 'Successfully logged in!' }),
+  });
+}
+
+window.fetch = mockFetch;
+
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+
+function submitForm() {
+  const email = document.querySelector('.email').value;
+  const password = document.querySelector('.password').value;
+  const emailError = document.querySelector('.emailError');
+
+  if (!validateEmail(email)) {
+      emailError.textContent = 'Please enter a valid email address';
+      return;
+  } else {
+      emailError.textContent = '';
+  }
+
+  const data = {
+      email: email,
+      password: password
+  };
+
+  const serverUrl = 'https://test.com';
+
+  fetch(`${serverUrl}/auth`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+      alert(data.message);
+      window.location.assign('https://example.com');
+  })
+  .catch(error => {
+      alert(error);
+  });
+}
+
+
+document.querySelector('.submit')
+  .addEventListener('click', submitForm);
